0165651a9ed4da521df916d76df1722e5b0e1727
lydiathornton <lydia.d.thornton@gmail.com>
2019-03-14 13:55:54 -0700

    initialized and set up store

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,15 @@
+
+
+
+const initialState = {
+    friends: []
+};
+
+const reducer = (statee = initialState, action) => {
+    switch(action.type) {
+        default:
+            return statee;
+    }
+}
+
+export default reducer;
